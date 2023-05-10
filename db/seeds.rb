34302763cd3c22c98797c2703d2ee5ed5a0b09b4
Eli <eghinshaw@gmail.com>
2023-05-10 12:53:12 -0500

    created chair product in seed, and desk too.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-product = Product.new(name: "Desk", price: 10, image_url:)
+product = Product.new(name: "Desk", price: 125, image_url: "https://unruhfurniture.com/wp-content/uploads/2018/02/Product-Desk-Olivia-Desk-No-Background-1-W1600.png", description: "This is a desk, you can put stuff on it")
+product.save
+
+product = Product.new(name: "Chair", price: 35, image_url: "https://assets.stickpng.com/images/580b57fcd9996e24bc43c26d.png", description: "This is a chair, you use it to sit down in a higher location(?)")
+product.save
