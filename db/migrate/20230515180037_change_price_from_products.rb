186c8f6e2da0240b4a256f10b5ed992487be290a
Eli <eghinshaw@gmail.com>
2023-05-15 13:10:57 -0500

    edited price datatype to use demical, added stock column

---
+++ db/migrate/20230515180037_change_price_from_products.rb
@@ -0,0 +1,6 @@
+class ChangePriceFromProducts < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+    add_column :products, :stock, :integer
+  end
+end
