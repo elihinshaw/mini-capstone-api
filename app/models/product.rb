12c4ae4ebc7f4e0853341e480ec3e0375dbc31ca
Eli <eghinshaw@gmail.com>
2023-05-16 11:38:12 -0500

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :description, length: { maximum: 500 }
+  validates :image_url, allow_blank: true, format: { with: %r{.(jpg|png)\Z}i }
+
   def is_discounted?
     return price <= 10
   end
