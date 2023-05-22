cf72ae996e038c5607534373bf105df5bf360aa3
Eli <eghinshaw@gmail.com>
2023-05-22 13:53:03 -0500

    index and show only show if logged in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,32 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = current_user.orders.all
     render :index
   end
 
   def show
-    @order = Order.find_by(id: params["id"])
+    @order = current_user.orders.find_by(id: params["id"])
     render :show
   end
 
   def create
     # if current_user
+    product = Product.find_by(id: params["product_id"])
+    quantity = params["quantity"].to_i
+
+    calculated_subtotal = product.price * quantity
+    calculated_tax = product.tax * quantity
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.create(
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: params["subtotal"],
-      tax: params["tax"],
-      total: params["total"],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
+
     if @order.valid?
       render :show
     else
