518de728f642bbbb1849b84371c794a134e0d8e1
Eli <eghinshaw@gmail.com>
2023-05-24 13:01:31 -0500

    added the model associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
+  has_many :carted_products
   # validates :user_id, presence: true
   # validates :product_id, presence: true
   # validates :quantity, presence: true
